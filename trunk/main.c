a4f8a83a4bb3ef96edc79bce3892f9df25193b34
mtj@mtjones.com <mtj@mtjones.com@20a78a83-fb2e-360a-ce79-ea74f8479c2d>
2014-02-06 03:58:47 +0000

    Updates.

--- trunk/main.c
+++ trunk/main.c
@@ -6,10 +6,57 @@
 #include <string.h>
 #include <stdio.h>
 
+#include "pigtypes.h"
+
 #define MAX_FILENAME	80
 
 int verbose = 0;
-char filename[MAX_FILENAME+1];
+char script_file[MAX_FILENAME+1]={0};
+
+int execute_script( char* script )
+{
+  FILE *fp = fopen(script, "r");
+  char line[256];
+  int  linenum=0;
+  int  ret;
+
+  if (strlen(script) == 0) 
+  {
+    printf("Must specify a script file.\n");
+    return -1;
+  }
+
+  fp = fopen(script, "r");
+
+  if (fp == NULL)
+  {
+    printf("Unable to open %s\n", script);
+    return -1;
+  }
+
+  while (fgets(line, 256, fp) != NULL)
+  {
+    // Simple parser for a very regular grammar
+    if      (strstr(line, "LOAD") != NULL)
+    {
+      ret = parseLoad( line );
+    }
+    else if (strstr(line, "DUMP") != NULL)
+    {
+      ret = parseDump( line );
+    }
+    else
+    {
+      printf("Cannot parse line %d\n", linenum);
+      ret = -1;
+      break;
+    }
+
+    linenum++;
+  }
+
+  return 0;
+}
 
 int main( int argc, char *argv[] )
 {
@@ -24,13 +71,13 @@ int main( int argc, char *argv[] )
         break;
 
       case 'f':
-        strncpy( filename, optarg, MAX_FILENAME );
-printf("input file is %s\n", filename);
+        strncpy( script_file, optarg, MAX_FILENAME );
         break;
 
     }
 
   }
 
-  return 0;
+  return execute_script( script_file );
 }
+
--- trunk/parser.c
+++ trunk/parser.c
@@ -4,4 +4,29 @@
 #include <stdio.h>
 #include "pigtypes.h"
 
+int parseLoad( char* line )
+{
+  char relation[MAX_RELATION_NAME];
+  char filename[MAX_FILENAME];
+  char delim;
+  int ret;
+  
+  ret = sscanf(line, "%s = LOAD %s DELIM '%c';", relation, filename, &delim);
+
+  printf(" relation %s, filename %s, delim %c\n", relation, filename, delim);
+
+  return ret;
+}
+
+int parseDump( char* line )
+{
+  char relation[MAX_RELATION_NAME];
+  int  ret;
+
+  ret = sscanf(line, "DUMP %s;", relation);
+
+  printf(" relation %s\n", relation);
+
+  return ret;
+}
 
